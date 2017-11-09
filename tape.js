72e6785c360b50112d15f0c87d2bad6b74005a2b
Ratheesh-gs <ratheeshbabu.m@greatinnovus.com>
2017-11-09 14:39:10 +0530

    Update tape.js

--- tape.js
+++ tape.js
@@ -8,7 +8,7 @@ test("parse request with digest auth", function (t) {
 		t.plan(4);
 
 			t.false(0, "err should be falsy");
-			t.equal(1,0, "err should be falsy");
+			t.equal(0,0, "equal");
 	t.ok('success');
 	t.skip('not processed')
 

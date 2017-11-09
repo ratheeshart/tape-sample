f4f62364a4a2bf9ab4b4c1fc44b943bc0b2114cf
Ratheesh-gs <ratheeshbabu.m@greatinnovus.com>
2017-11-09 14:37:35 +0530

    Update tape.js

--- tape.js
+++ tape.js
@@ -2,6 +2,8 @@ var test = require("tape");
 //var HttpStatus = require('http-status-codes');
 var httpMock = require("node-mocks-http");
 //var _ = require("lodash");
+try{
+	
 test("parse request with digest auth", function (t) {
 		t.plan(4);
 
@@ -11,3 +13,8 @@ test("parse request with digest auth", function (t) {
 	t.skip('not processed')
 
 });
+}
+catch(v){
+ console.log(v);
+}
+ 

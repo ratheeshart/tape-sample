ad4762cb7cd85a42fc42d4e8bc4a139c77e49a89
Ratheesh-gs <ratheeshbabu.m@greatinnovus.com>
2017-11-09 14:01:35 +0530

    Create tape.js

---
+++ tape.js
@@ -0,0 +1,11 @@
+var test = require("tape");
+var HttpStatus = require('http-status-codes');
+var httpMock = require("node-mocks-http");
+var _ = require("lodash");
+test("parse request with digest auth", function (t) {
+		t.plan(4);
+
+			t.false(0, "err should be falsy");
+			t.equal(1,1, "err should be falsy");
+
+});
